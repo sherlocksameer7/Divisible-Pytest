b1593b4a21c7e1d759d7fb1a8b7bd35f5e3af64a
sherlocksameer7 <arijitsameers@gmail.com>
2022-03-28 12:35:03 +0530

    Changes with fixture concept

--- test_Divisible.py
+++ test_Divisible.py
@@ -1,26 +1,32 @@
 import Divisible
 
+import pytest
 
-def test_divisible_by_5():
+@pytest.fixture   # fixture is not a function. so, dont use () after that !!!
+def input():
+    x = 40
+    return x
 
-    a = 35
-    Res = Divisible.divisible_by_5(a)
+def test_divisible_by_5(input):
+
+    # a = 35
+    Res = Divisible.divisible_by_5(input)
     assert Res == True
 
-def test_divisible_by_7():
+def test_divisible_by_7(input):
 
-    a = 49
-    Res = Divisible.divisible_by_7(a)
-    assert Res == True
+    # a = 49
+    Res = Divisible.divisible_by_7(input)
+    assert Res == False   # Here we change True or False to get the test cases passes !!!
 
-def test_divisible_by_9():
+def test_divisible_by_9(input):
 
-    a = 45
-    Res = Divisible.divisible_by_9(a)
-    assert Res == True
+    # a = 45
+    Res = Divisible.divisible_by_9(input)
+    assert Res == False
 
-def test_divisible_by_10():
+def test_divisible_by_10(input):
 
-    a = 70
-    Res = Divisible.divisible_by_10(a)
+    # a = 70
+    Res = Divisible.divisible_by_10(input)
     assert Res == True
